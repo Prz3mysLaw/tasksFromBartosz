760c1564680411779ff0a845428e0afcc835860b
bboyKamel <nacoon@tlen.pl>
2019-03-21 18:19:47 +0100

    Create answer - PingPong class

---
+++ src/main/java/pl/com/urestApp/maybeMyNewJob/pingpong/PingPong.java
@@ -0,0 +1,8 @@
+package pl.com.urestApp.maybeMyNewJob.pingpong;
+
+import lombok.Getter;
+
+@Getter
+public class PingPong {
+    private static final String answer = "<h1 style=\"text-align: center; color: green;\">pong</h1>";
+}
