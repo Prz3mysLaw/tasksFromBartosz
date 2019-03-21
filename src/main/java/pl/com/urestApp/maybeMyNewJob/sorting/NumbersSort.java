aa17a049a8962791d84d342df9851f9d01b126b4
bboyKamel <nacoon@tlen.pl>
2019-03-21 19:07:54 +0100

    Create NumbersSort class

---
+++ src/main/java/pl/com/urestApp/maybeMyNewJob/sorting/NumbersSort.java
@@ -0,0 +1,16 @@
+package pl.com.urestApp.maybeMyNewJob.sorting;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+
+@Data
+public class NumbersSort {
+
+    ArrayList<Long> numbersList = new ArrayList();
+    String sortType;
+
+}
+
+
+
