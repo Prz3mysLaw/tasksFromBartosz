ff84aeb9f8c14094db998551feccb566d17c58b6
bboyKamel <nacoon@tlen.pl>
2019-03-22 09:47:16 +0100

    Create class for input JSOON - currency

---
+++ src/main/java/pl/com/urestApp/maybeMyNewJob/maybeKantor/InputCurrencyCode.java
@@ -0,0 +1,10 @@
+package pl.com.urestApp.maybeMyNewJob.maybeKantor;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class InputCurrencyCode {
+    String currency;
+}
