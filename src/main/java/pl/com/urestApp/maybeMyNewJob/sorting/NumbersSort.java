14acdbf59df3093caf212ad4aef588a9b470fc24
bboyKamel <nacoon@tlen.pl>
2019-03-22 12:41:50 +0100

    catch NullPointer

--- src/main/java/pl/com/urestApp/maybeMyNewJob/sorting/NumbersSort.java
+++ src/main/java/pl/com/urestApp/maybeMyNewJob/sorting/NumbersSort.java
@@ -1,20 +1,24 @@
 package pl.com.urestApp.maybeMyNewJob.sorting;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Value;
 
 import java.util.ArrayList;
 import java.util.Collections;
 
+
 @Data
 public class NumbersSort {
 
-    ArrayList<Long> numbersList = new ArrayList();
-    String sortType;
+    ArrayList<Long> numbers = new ArrayList();
+    String order;
 
 
-    public ArrayList timeToSort(ArrayList nl, String orderType) {
-
+    public ArrayList timeToSort(ArrayList nl, String orderType) throws NullPointerException {
         ArrayList empty = new ArrayList();
+        try {
+
         if(orderType == null || nl == null) return empty;
 
         if(orderType.toUpperCase().trim().contains("ASC") && orderType.trim().length() == 3) {
@@ -25,7 +29,7 @@ public class NumbersSort {
         if(orderType.toUpperCase().trim().contains("DESC") && orderType.trim().length() == 4) {
             Collections.sort(nl, Collections.reverseOrder());
             return nl;
-        }
+        } } catch (NullPointerException e) {}
 
         return empty;
     }
