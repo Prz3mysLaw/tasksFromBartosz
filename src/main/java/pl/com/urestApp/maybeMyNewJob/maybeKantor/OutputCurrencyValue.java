209ba27b0209639e9b363bed6c234ff27c81ee37
bboyKamel <nacoon@tlen.pl>
2019-03-22 09:48:52 +0100

    Create class for output JSOON - currency value

---
+++ src/main/java/pl/com/urestApp/maybeMyNewJob/maybeKantor/OutputCurrencyValue.java
@@ -0,0 +1,11 @@
+package pl.com.urestApp.maybeMyNewJob.maybeKantor;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class OutputCurrencyValue {
+    String value;
+}
